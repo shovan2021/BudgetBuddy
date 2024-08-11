f6e20efc3ba196c894c7e43c85cbe196ec717615
Shovan Kar <shovankar.dev@gmail.com>
2024-08-11 21:19:31 +0530

    Change DB from MySQL to MongoDB and configure session

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -110,6 +110,12 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+        ],
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://bbuser:RJX7h1iW1HnHOzLV@bbcluster0.o7gze.mongodb.net/'),
+            'database' => 'budget',
         ],
 
     ],
